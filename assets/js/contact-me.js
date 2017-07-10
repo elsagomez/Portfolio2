de1939509d4d098b41b8cb61bd578e279f73904a
elsagomez <elsa.ghdz@gmail.com>
2017-07-10 09:43:35 -0400

    contact form

---
+++ assets/js/contact-me.js
@@ -0,0 +1,36 @@
+var config = {
+    apiKey: "AIzaSyAnF9OklELLx-GH4g9gSHPpWe66f7hucKk",
+    authDomain: "portfolio-4d2a3.firebaseapp.com",
+    databaseURL: "https://portfolio-4d2a3.firebaseio.com",
+    projectId: "portfolio-4d2a3",
+    storageBucket: "portfolio-4d2a3.appspot.com",
+    messagingSenderId: "394562979074"
+};
+firebase.initializeApp(config);
+
+var database = firebase.database();
+
+$("#submit").click(function(event) {
+    event.preventDefault();
+    var name = $("input#name").val();
+    var email = $("input#email").val();
+    var phone = $("input#phone").val();
+    var message = $("textarea#message").val();
+
+
+
+    var newContact = {
+        name: name,
+        email: email,
+        phone: phone,
+        message: message,
+
+
+    }
+
+    database.ref().push(newContact);
+
+
+
+
+});
